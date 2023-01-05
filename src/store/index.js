b2a3252e1533de5d696bc07d6373ccbd07ac23da
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-05 16:49:58 +0000

    Add actions to test dispatch

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,8 @@ const songsSlice = createSlice({
   initialState: [],
   reducers: {
     addSong(state, action) {
+      // State referred to over here does not correspond to the big state object of the application
+      // It only refers to the songs slice
       state.push(action.payload);
     },
     removeSong(state, action) {
@@ -23,7 +25,7 @@ const startingState = store.getState();
 
 console.log(startingState);
 
-store.dispatch({ type: 'song/addSong', payload: 'New song!'});
+store.dispatch(songsSlice.actions.addSong('New Song!'));
 
 const finalState = store.getState();
 
