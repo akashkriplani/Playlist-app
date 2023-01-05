75d1d71a32728a661c469e63fa54b72c991f36e0
Akash Kriplani <aakash23kriplani@gmail.com>
2023-01-05 17:24:16 +0000

    Implement add movie and remove movie in movie playlist component

--- src/store/index.js
+++ src/store/index.js
@@ -17,12 +17,32 @@ const songsSlice = createSlice({
   },
 });
 
+const moviesSlice = createSlice({
+  name: "movie",
+  initialState: [],
+  reducers: {
+    addMovie(state, action) {
+      // State referred to over here does not correspond to the big state object of the application
+      // It only refers to the movies slice
+      state.push(action.payload);
+    },
+    removeMovie(state, action) {
+      // action.payload will be the name of the movie which needs to be removed, ergo, a string.
+      const index = state.indexOf(action.payload);
+      state.splice(index, 1);
+    },
+  },
+});
+
+
 const store = configureStore({
   reducer: {
     songs: songsSlice.reducer,
+    movies: moviesSlice.reducer
   },
 });
 
 export { store };
 
 export const { addSong, removeSong } = songsSlice.actions;
+export const { addMovie, removeMovie } = moviesSlice.actions;
